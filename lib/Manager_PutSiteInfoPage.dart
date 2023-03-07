a6ef2851961e129cb7e379df3ca883b98065c0b9
haZuny <gkwns5791@naver.com>
2023-03-07 21:00:12 +0900

    개발중

--- lib/Manager_PutSiteInfoPage.dart
+++ lib/Manager_PutSiteInfoPage.dart
@@ -17,7 +17,8 @@ class PutSiteInfoPageManager extends StatefulWidget {
 
 class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
   @override
-  Widget build(BuildContext context) => GestureDetector(
+  Widget build(BuildContext context) =>
+      GestureDetector(
         onTap: () {
           FocusManager.instance.primaryFocus?.unfocus(); // 키보드 닫기 이벤트
         },
@@ -29,6 +30,7 @@ class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
+
                   /// 간격
                   Container(
                     height: getFullScrennSizePercent(
@@ -55,7 +57,7 @@ class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
                         // 힌트
                         hintText: "Workspace name",
                         hintStyle:
-                            TextStyle(color: Color(themaColor_whiteBlack)),
+                        TextStyle(color: Color(themaColor_whiteBlack)),
                         // 색상(설정 안하면 그림자에 먹힘)
                         filled: true,
                         fillColor: Colors.white,
@@ -65,14 +67,14 @@ class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
                                 color: Color(themaColor_whiteBlack),
                                 width: allPage_btnBorderWidth),
                             borderRadius:
-                                BorderRadius.circular(allPage_TFRadius)),
+                            BorderRadius.circular(allPage_TFRadius)),
                         // 선택 됐을때 테두리
                         focusedBorder: OutlineInputBorder(
                             borderSide: BorderSide(
                                 color: Color(themaColor_whiteBlack),
                                 width: allPage_btnBorderWidth),
                             borderRadius:
-                                BorderRadius.circular(allPage_TFRadius)),
+                            BorderRadius.circular(allPage_TFRadius)),
                       ),
                     ),
                     // 그림자
@@ -100,7 +102,7 @@ class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
                         // 힌트
                         hintText: "Address",
                         hintStyle:
-                            TextStyle(color: Color(themaColor_whiteBlack)),
+                        TextStyle(color: Color(themaColor_whiteBlack)),
                         // 색상(설정 안하면 그림자에 먹힘)
                         filled: true,
                         fillColor: Colors.white,
@@ -110,14 +112,14 @@ class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
                                 color: Color(themaColor_whiteBlack),
                                 width: allPage_btnBorderWidth),
                             borderRadius:
-                                BorderRadius.circular(allPage_TFRadius)),
+                            BorderRadius.circular(allPage_TFRadius)),
                         // 선택 됐을때 테두리
                         focusedBorder: OutlineInputBorder(
                             borderSide: BorderSide(
                                 color: Color(themaColor_whiteBlack),
                                 width: allPage_btnBorderWidth),
                             borderRadius:
-                                BorderRadius.circular(allPage_TFRadius)),
+                            BorderRadius.circular(allPage_TFRadius)),
                       ),
                       // 수정 불가 설정
                       readOnly: true,
@@ -128,7 +130,7 @@ class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
                             isScrollControlled: true,
                             shape: RoundedRectangleBorder(
                               borderRadius:
-                                  BorderRadius.circular(allPage_bigBoxRadious),
+                              BorderRadius.circular(allPage_bigBoxRadious),
                             ),
                             context: context,
                             builder: (context) => SiteInfoBottomSheet());
@@ -159,7 +161,7 @@ class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
                         // 힌트
                         hintText: "Address detail",
                         hintStyle:
-                            TextStyle(color: Color(themaColor_whiteBlack)),
+                        TextStyle(color: Color(themaColor_whiteBlack)),
                         // 색상(설정 안하면 그림자에 먹힘)
                         filled: true,
                         fillColor: Colors.white,
@@ -169,14 +171,14 @@ class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
                                 color: Color(themaColor_whiteBlack),
                                 width: allPage_btnBorderWidth),
                             borderRadius:
-                                BorderRadius.circular(allPage_TFRadius)),
+                            BorderRadius.circular(allPage_TFRadius)),
                         // 선택 됐을때 테두리
                         focusedBorder: OutlineInputBorder(
                             borderSide: BorderSide(
                                 color: Color(themaColor_whiteBlack),
                                 width: allPage_btnBorderWidth),
                             borderRadius:
-                                BorderRadius.circular(allPage_TFRadius)),
+                            BorderRadius.circular(allPage_TFRadius)),
                       ),
                     ),
                     // 그림자
@@ -219,8 +221,11 @@ class _PutSiteInfoPageManager extends State<PutSiteInfoPageManager> {
 
 /// 바텀 시트
 class SiteInfoBottomSheet extends StatelessWidget {
+  
+
   @override
-  Widget build(BuildContext context) => GestureDetector(
+  Widget build(BuildContext context) =>
+      GestureDetector(
         onTap: () {
           FocusManager.instance.primaryFocus?.unfocus(); // 키보드 닫기 이벤트
         },
@@ -228,7 +233,10 @@ class SiteInfoBottomSheet extends StatelessWidget {
           padding: EdgeInsets.all(getFullScrennSizePercent(
               context, putSiteInfoPage_containerPadding)),
           // 크기 설정
-          height: MediaQuery.of(context).size.height *
+          height: MediaQuery
+              .of(context)
+              .size
+              .height *
               putSiteInfoPage_bottomsheetHeight,
           child: Column(
             children: [
@@ -240,7 +248,7 @@ class SiteInfoBottomSheet extends StatelessWidget {
                       decoration: InputDecoration(
                           hintText: 'Address',
                           hintStyle:
-                              TextStyle(color: Color(themaColor_whiteBlack))),
+                          TextStyle(color: Color(themaColor_whiteBlack))),
                     ),
                   ),
                   // Search 버튼
@@ -248,11 +256,12 @@ class SiteInfoBottomSheet extends StatelessWidget {
                       onPressed: () async {
                         print(_addr1Controller.text);
 
-                        String _AddrSearchURL="http://dapi.kakao.com/v2/local/search/address.json?query=";
-                        String _AddrSearchKey="93349143720fbaa860b00f8191e7f2de";
+                        String _AddrSearchURL = "http://dapi.kakao.com/v2/local/search/address.json?query=";
+                        String _AddrSearchKey = "93349143720fbaa860b00f8191e7f2de";
 
                         Dio dio = Dio();
-                        dio.options.headers = {'Authorization': 'KakaoAK ' + _AddrSearchKey};
+                        dio.options.headers =
+                        {'Authorization': 'KakaoAK ' + _AddrSearchKey};
 
                         var res = await dio.get(_AddrSearchURL + '순천향로 22');
                         print(res.data);
@@ -268,21 +277,7 @@ class SiteInfoBottomSheet extends StatelessWidget {
               Expanded(
                 child: ListView(
                   children: [
-                    GestureDetector(
-                      child: Padding(
-                        padding: EdgeInsets.only(
-                            top: putSiteInfoPage_siteListTileFontPadding,
-                            bottom: putSiteInfoPage_siteListTileFontPadding),
-                        child: Text(
-                          "충남 아산시 순천향로 23",
-                          style: TextStyle(
-                              fontSize: putSiteInfoPage_siteListTileFontSize),
-                        ),
-                      ),
-                      onTap: () {
-                        print(_addr1Controller.text);
-                      },
-                    ),
+                    AddrSearchListTile('ㅁㄴㅇㄹ')
                   ],
                 ),
               )
@@ -292,6 +287,30 @@ class SiteInfoBottomSheet extends StatelessWidget {
       );
 }
 
+/// 주소 검색 리스트 타일
+class AddrSearchListTile extends StatelessWidget {
+  String addr = '';
+
+  AddrSearchListTile(String addr) : this.addr = addr;
+
+  @override
+  Widget build(BuildContext context) =>
+      GestureDetector(
+        child: Padding(
+          padding: EdgeInsets.only(
+              top: putSiteInfoPage_siteListTileFontPadding,
+              bottom: putSiteInfoPage_siteListTileFontPadding),
+          child: Text(
+            this.addr,
+            style: TextStyle(
+                fontSize: putSiteInfoPage_siteListTileFontSize),
+          ),
+        ),
+        onTap: () {
+          print(_addr1Controller.text);
+        },
+      );
+}
 
 
 /// 주소 검색 함수
